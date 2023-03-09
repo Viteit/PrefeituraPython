e9058b3d1c0ffa800de394f55f0b2fe05f61033f
Thiago Santos <thiagosouzasantos@gmail.com>
2023-03-09 18:56:12 -0300

    [BUGFIX] Correction bug

--- teste.py
+++ teste.py
@@ -1,1 +1,11 @@
-print('Hello World')
+print("Linguagens permitidas:")
+print("EN-English")
+print("PT-Portuguese")
+response=input("Digite a opção: ")
+
+if (response.upper()=="EN"):
+    print('Hello World')
+elif (response.upper()=="PT"):
+    print('Olá mundo !!')
+else:
+    print('Linguagem não conhecida !')
